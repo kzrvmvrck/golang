527ec11a2756e0c2e64f9219ee2a1ee6083875ee
kzrvmvrck <kzrv.maverick@gmail.com>
2025-01-05 14:43:30 +0300

    Learn with ChatGPT

--- third_stage/syntax_golang/Arrays/arrays.go
+++ third_stage/syntax_golang/Arrays/arrays.go
@@ -7,6 +7,14 @@ func modifityArrays(arr [3]int) []int {
 	return arr[:]
 }
 
+func arrSum(arr []int) int {
+	result := 0
+	for _, value := range arr {
+		result += value
+	}
+	return result
+}
+
 func main() {
 	// Массив нумерованная последовательность элементов одного типа с фиксированной длиной
 	var a [5]int
@@ -58,5 +66,42 @@ func main() {
 	arrTestInFunc := [3]int{1, 2, 3}
 	arrTestInFuncOne := modifityArrays(arrTestInFunc)
 	fmt.Println(arrTestInFuncOne, arrTestInFunc)
+
+	for index, value := range myArrAuto {
+		fmt.Printf("value on index %d is: %d \n", index, value)
+	}
+
+	var x [5]int
+	fmt.Println(x)
+
+	x[3] = 666
+	fmt.Println(x, len(x))
+	fmt.Println(len(x))
+
+	var xCode [58]string
+	fmt.Println(xCode, len(xCode))
+
+	for i := 65; i <= 122; i++ { //	65 индекс в таблице UTF это буква A
+		xCode[i-65] = string(i)
+	}
+
+	fmt.Println(xCode)
+	fmt.Println(xCode[42])
+
+	// Опа ча! в Golang нет методов для  массивов, но можно определять функции для них
+
+	// 1 Этап делаем функцию которая примет в аргументы Наш Массив. Вне функции main
+	// arrSum
+
+	// 2 Этап создаем массив
+	testArr := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	result := arrSum(testArr[:])
+	fmt.Println(result)
+
+	rvsk_1000_number_14 := []int{}
+	fmt.Println(rvsk_1000_number_14)
+
+	//Сравнение массивов
 
 }
